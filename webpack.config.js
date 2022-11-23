9a0a8aeba66c6a843aac3ff4f67489f007a15e32
Nannersaucer <118583843+Nannersaucer@users.noreply.github.com>
2022-11-23 23:11:15 +0000

    adding webpack and vue

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const config = {
+    mode: 'development',
+    entry: {
+        'home': './public/client/home.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './public/compiled_js'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: [
+            '.js',
+            '.vue'
+        ]
+    }, 
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    
+}
+
+module.exports = config;
+
