556bec96b6cc0e71438aeeaa4ee195827effb9d6
Nannersaucer <118583843+Nannersaucer@users.noreply.github.com>
2022-11-24 02:36:14 +0000

    updating vue to use syntax in line w new version

--- public/client/home.js
+++ public/client/home.js
@@ -1,16 +1,9 @@
-import Vue from "vue";
+import { createApp } from 'vue'
 
-let app = new Vue({
-    el: '#home',
-    data: {
-        names: ['Wisdom', "Ekpot", "Ubongabasi"]
-    },
-    methods: {
-        logSomeThing() {
-            return 'Hello Wisdom Ekpot'
-        }
-    },
-    mounted() {
-        console.log(this.logSomeThing());
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
     }
-})
+  }).mount('#home')
